51d9fffacfdd09289b18fb018ae2212dc7a932be
AlexandraPankratova <ablackcat@yandex.ru>
2021-03-16 16:13:34 +0300

    Apply proper variable names

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ import pandas as pd
 import xlrd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-now = datetime.datetime.now()
-word = ''
-year_est = 1920
+current_date = datetime.datetime.now()
+right_term_for_year = ''
+company_age = current_date.year - 1920
 
-if (now.year - year_est)%10 == 1:
-    word = 'год'
-elif (now.year - year_est)%10 == 2 or (now.year - year_est)%10 == 3 or (now.year - year_est)%10 == 4:
-    word = 'года'
-elif (now.year - year_est)%10 == 0 or (now.year - year_est)%10 == 5 or (now.year - year_est)%10 == 6 or (now.year - year_est)%10 == 7 or (now.year - year_est)%10 == 8 or (now.year - year_est)%10 == 9:
-    word = 'лет'
+if company_age%10 == 1:
+    right_term_for_year = 'год'
+elif company_age%10 == 2 or company_age%10 == 3 or company_age%10 == 4:
+    right_term_for_year = 'года'
+elif company_age%10 == 0 or company_age%10 == 5 or company_age%10 == 6 or company_age%10 == 7 or company_age%10 == 8 or company_age%10 == 9:
+    right_term_for_year = 'лет'
 
-data = pd.read_excel('Shop_stock.xlsx', na_values='nan', keep_default_na=False)
+shop_stock = pd.read_excel('Shop_stock.xlsx', na_values='nan', keep_default_na=False)
 
-wines = data.to_dict(orient='records')
+wines = shop_stock.to_dict(orient='records')
 
-new_dict = cl.defaultdict(list)
+categorised_shop_stock = cl.defaultdict(list)
 
 for drinks in wines:
-    new_dict[drinks.get('Категория')].append(drinks)
+    categorised_shop_stock[drinks.get('Категория')].append(drinks)
 
-pprint (new_dict)
+pprint (categorised_shop_stock)
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -37,10 +37,10 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    age = now.year - year_est,
-    correct_word = word,
+    age = company_age,
+    year = right_term_for_year,
     wines = wines,
-    collection = sorted(new_dict.keys())
+    collection = sorted(categorised_shop_stock.keys())
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
